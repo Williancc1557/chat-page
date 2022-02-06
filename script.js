312fea3e5f8c7704cb1b28c8b1bb4bceaabd4237
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-02-06 15:53:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const socket = io.connect('localhost:3000', { transports: ['websocket'] });
+
+const renderMessage = (message) => {
+    $(".messages").append('<div class="message"><strong>' + message.author + '</strong>: ' + message.message + '</div>')
+}
+socket.on("previusMessage", (message) => {
+    for (message of message) {
+        renderMessage(message)
+    }
+})
+socket.on("receivedMessage", (message) => {
+    renderMessage(message)
+})
+
+$("#chat").submit(event => {
+    event.preventDefault();
+
+    const author = $("input[name=username]").val()
+    const message = $("input[name=message]").val()
+
+    if (author.length && message.length) {
+        const messageConfig = {
+            author: author,
+            message: message,
+        }
+
+        renderMessage(messageConfig)
+
+        socket.emit('sendMessage', messageConfig)
+    }
+})
