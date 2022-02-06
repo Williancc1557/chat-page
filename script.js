6416920dd5bab6bba314cf24e765d8cf4f42711d
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-02-06 18:50:34 -0300

    Input message was been updated

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const socket = io.connect('https://api-chat-page.herokuapp.com', { transports: ['websocket'] });
+$(".messages").animate({ scrollTop: 20000000 }, "slow");
+
+/* const socket = io.connect('https://api-chat-page.herokuapp.com', { transports: ['websocket'] });
+ */
+
+const socket = io.connect('http://localhost:3000', { transports: ['websocket'] });
 
 const renderMessage = (message) => {
     $(".messages").append('<div class="message"><strong>' + message.author + '</strong>: ' + message.message + '</div>')
-}
+};
+
+
 socket.on("previusMessage", (message) => {
     for (message of message) {
         renderMessage(message)
-    }
-})
+    };
+});
 socket.on("receivedMessage", (message) => {
-    renderMessage(message)
-})
+    renderMessage(message);
+});
 
 $("#chat").submit(event => {
     event.preventDefault();
+    $(".messages").animate({ scrollTop: 20000000 }, "slow");
 
-    const author = $("input[name=username]").val()
-    const message = $("input[name=message]").val()
+    const author = $("input[name=username]").val();
+    const message = $("input[name=message]").val();
 
     if (author.length && message.length) {
         const messageConfig = {
             author: author,
             message: message,
-        }
+        };
 
-        renderMessage(messageConfig)
-
-        socket.emit('sendMessage', messageConfig)
-    }
+        renderMessage(messageConfig);
+        document.querySelector("input[name=message]").value = "";
+        socket.emit('sendMessage', messageConfig);
+    };
 })
